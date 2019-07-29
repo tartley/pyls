e391c7a946538c668f217bd3acd575461dd8613b
Jonathan Hartley <tartley@tartley.com>
2019-07-28 22:45:02 -0500

    more fixes, but still doesn't work

--- setup.py
+++ setup.py
@@ -13,15 +13,21 @@ def main():
     setup(
         name=NAME,
         version=version,
+        python_requires="<3.0.0",
+        packages=['pyls'],
         entry_points={
             'console_scripts': [
-                'pyls=pyls.main.main',
+                'pyls=pyls.main:main',
             ],
         },
         author='Jonathan Hartley',
         author_email='tartley@tartley.com',
         url='https://github.com/tartley/pyls',
         license='New BSD License',
+        classifiers=[
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+        ],
     )
 
 
