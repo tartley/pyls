81cd647a007e6403ad1cd67e2fd4aec6a75a7ea6
Jonathan Hartley <tartley@tartley.com>
2019-07-28 22:32:22 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
-
 import sys
 
+from setuptools import setup
 
 NAME = 'pyls'
 SCRIPT = 'pyls.bat'
 
 
 def main():
-    from setup_utils import distribute_setup, get_config
-    distribute_setup.use_setuptools()
-
-    from setuptools import setup
-
     if 'py2exe' in sys.argv:
         import py2exe
-
     version = __import__(NAME).__version__
-    config = get_config(NAME, version, SCRIPT, console=True)
-    config.update(dict(
+    setup(
+        name=NAME,
+        version=version,
+        entry_points={
+            'console_scripts': [
+                'pyls=pyls.main.main',
+            ],
+        },
         author='Jonathan Hartley',
         author_email='tartley@tartley.com',
         url='https://github.com/tartley/pyls',
         license='New BSD License',
-    ) )
-
-    setup(**config)
+    )
 
 
 if __name__ == '__main__':
